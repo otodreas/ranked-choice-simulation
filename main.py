0f8385e3c4fb733e3c70c55d09ab982072bb5905
Oliver Todreas <otodreas@gmail.com>
2026-06-14 19:24:00 -0400

    comment

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import uuid
 
 import numpy as np
+from numpy.typing import NDArray
 
 np.random.seed(0)
 
-CANDIDATES = np.array(["A", "B", "C", "D"])
-N_BALLOTS: int = 1_000
+CANDIDATES: dict[str, float] = {
+    "A": 1,
+    "B": 2,
+    "C": 3,
+    "D": 4,
+}
+N_BALLOTS_CAST: int = 1_000
 
 
 class Ballot:
-    def __init__(self, candidates):
-        self.candidates = candidates
+    def __init__(self, candidates: dict[str, float]):
         self.valid = True
+        self.candidates = list(candidates.keys())
+
+        # Normalize candidate win probabilities
+        p_raw = np.array(list(candidates.values()))
+        self.candidate_win_probabilities = p_raw / np.sum(p_raw)
 
     def rank_ballot(self):
+        """Simulate an individual vote"""
         self.candidates_ranked = np.random.choice(
             self.candidates,
             np.random.randint(1, len(self.candidates)),
             replace=False,
+            p=self.candidate_win_probabilities,
         )
 
     def get_top_vote(self):
-        # TODO: handle missing ranking
         if len(self.candidates_ranked):
             self.top_vote = self.candidates_ranked[0]
         else:
@@ -41,7 +52,7 @@ class Election:
 
     def determine_winner(self):
         self.results = {}
-        for ballot in self.votes.values():  # TODO: handle missing ballots
+        for ballot in self.votes.values():
             ballot.get_top_vote()
             if ballot.top_vote not in self.results.keys():
                 self.results[ballot.top_vote] = 1
@@ -49,7 +60,7 @@ class Election:
                 self.results[ballot.top_vote] += 1
 
         self.results = {
-            candidate: n_votes / len(self.votes.values())
+            candidate: np.float64(n_votes / len(self.votes.values()))
             for candidate, n_votes in self.results.items()
         }
         self.loser = min(self.results, key=self.results.get)
@@ -57,20 +68,24 @@ class Election:
         self.majority_winner = self.results[self.winner] > 0.5
 
     def rerank(self):
+        """Recount votes after the losing candidate has been eliminated"""
         self.eliminated_candidates.append(self.loser)
         discard_voter_ids = []
         for voter_id, ballot in self.votes.items():
             ballot.get_top_vote()
+
+            # Only process ballots whose top choice is the eliminated candidate
             if ballot.top_vote in self.eliminated_candidates:
                 ballot.remove_top_vote()
                 ballot.get_top_vote()
-                while (
-                    ballot.top_vote in self.eliminated_candidates
-                    and ballot.valid == True
-                ):
+
+                # Eliminate 2nd, 3rd choice, and so on if those are also eliminated
+                while ballot.top_vote in self.eliminated_candidates and ballot.valid:
                     ballot.remove_top_vote()
                     ballot.get_top_vote()
-                if ballot.valid == True:
+
+                # Update top vote or discard ballot if no more votes
+                if ballot.valid:
                     self.votes[voter_id] = ballot
                 else:
                     discard_voter_ids.append(voter_id)
@@ -80,23 +95,45 @@ class Election:
 
 
 def simulate_ballot_casting(
-    election: Election, candidates: np.array = CANDIDATES, n_ballots: int = N_BALLOTS
+    election: Election,
+    candidates: NDArray = CANDIDATES,
+    n_ballots: int = N_BALLOTS_CAST,
 ):
+    """Simulate the casting of ballots, save voter id"""
     for voter in range(n_ballots):
         ballot = Ballot(candidates)
         ballot.rank_ballot()
         election.submit_ballot(ballot, voter_id=uuid.uuid4())
 
 
+def print_results(election: Election):
+    for candidate, percent in election.results.items():
+        print(f"{candidate}: {np.round(percent * 100, 1)}%")
+
+
 def main():
+    # Assign new election
     election = Election()
+
+    # Simulate ballot casting and determine the winner
+    print("Starting ranked choice election simulation")
     simulate_ballot_casting(election)
     election.determine_winner()
+    
+    print("After round 0:")
+    print_results(election)
+    
+    # Start instant runnoff/ranked choice voting until a majority winner is declared
+    i = 0
     while not election.majority_winner:
+        i += 1
         election.rerank()
         election.determine_winner()
 
-    return election.winner
+        print(f"Eliminating {election.loser}\nAfter round {i}:")
+        print_results(election)
+
+    print(f"Winner: {election.winner} after {i} rounds")
 
 
 if __name__ == "__main__":
